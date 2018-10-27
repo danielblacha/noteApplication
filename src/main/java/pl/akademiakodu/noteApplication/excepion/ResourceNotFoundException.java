1baab2fdf7e769be281fe9e2b099f2778ece8d0e
danielblacha <daniel.blacha@gmail.com>
2018-10-27 12:12:56 +0200

    create exception

---
+++ src/main/java/pl/akademiakodu/noteApplication/excepion/ResourceNotFoundException.java
@@ -0,0 +1,30 @@
+package pl.akademiakodu.noteApplication.excepion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fileName;
+    private Object fieldValue;
+
+    private ResourceNotFoundException(String resourceName, String fileName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fileName, fieldValue));
+        this.resourceName = resourceName;
+        this.fileName = fileName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
