f99b33d3804f81a01c56e3968496a58b6256c284
danielblacha <daniel.blacha@gmail.com>
2018-10-27 11:58:58 +0200

    create model

---
+++ src/main/java/pl/akademiakodu/noteApplication/model/Note.java
@@ -0,0 +1,53 @@
+package pl.akademiakodu.noteApplication.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creatAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatAt() {
+        return creatAt;
+    }
+
+    public void setCreatAt(Date creatAt) {
+        this.creatAt = creatAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
